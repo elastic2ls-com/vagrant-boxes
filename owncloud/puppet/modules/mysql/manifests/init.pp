500e41908e3c6ae75c7a1ea49f2aefd6e4fe70ae
elastic2ls-awiechert <awiechert@elastic2ls.com>
2015-07-16 16:25:54 +0200

    minor changes to puppet configuration

---
+++ owncloud/puppet/modules/mysql/manifests/init.pp
@@ -0,0 +1,26 @@
+class mysql {
+
+	exec { 'apt-get update':
+  		path => '/usr/bin',
+	}
+
+	# Install mysql
+	package { ['mysql-server']:
+    		ensure => present,
+    		require => Exec['apt-get update'],
+	}
+	# Run mysql
+	service { 'mysql':
+    		ensure  => running,
+    		require => Package['mysql-server'],
+	}
+
+	# We set the root password here
+	exec { 'set-mysql-password':
+    		unless  => 'mysqladmin -uroot -proot status',
+    		command => "mysqladmin -uroot password root",
+    		path    => ['/bin', '/usr/bin'],
+    	require => Service['mysql'];
+	}
+
+}
