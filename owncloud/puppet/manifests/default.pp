a18aa00339e3e9857799cb875bb532c7a0c90750
elastic2ls-awiechert <awiechert@elastic2ls.com>
2015-07-16 10:01:44 +0200

    added owncloud

---
+++ owncloud/puppet/manifests/default.pp
@@ -0,0 +1,12 @@
+node "owncloud.t-mobile.de" {
+
+file { '/root/example_file.txt':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "700",
+    content => "Congratulations!
+Puppet has created this file.
+",}
+
+}
